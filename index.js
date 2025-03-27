d1dfcaa68fe2f0d82d3aa506c4804b3466564ce0
Jinnapat-232 <jinnapat.thongbanguay@g.swu.ac.th>
2025-03-28 00:08:15 +0700

    2

--- index.js
+++ index.js
@@ -92,3 +92,9 @@ app.get('/bookmarks', authController.authenticate, taskController.getBookmarks);
 app.post('/bookmarks/add', authController.authenticate, taskController.addBookmark);
 app.post('/bookmarks/remove', authController.authenticate, taskController.removeBookmark);
 
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
